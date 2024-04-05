83ab9850600ba17fc27ad70f8489b0cf8928d183
DianaKryzhanivska <di_parkhomenko@ukr.net>
2024-04-05 17:53:54 +0300

    getNearestStores ctrl

--- controllers/ePharmacy.js
+++ controllers/ePharmacy.js
@@ -1,3 +1,4 @@
+const { NearestStore } = require("../models/nearestStores");
 const { Store } = require("../models/stores");
 const ctrlWrapper = require("../services/ctrlWrapper");
 
@@ -7,6 +8,13 @@ const getAllStores = async (req, res) => {
   res.json(result);
 };
 
+const getNearestStores = async (req, res) => {
+  let filter = {};
+  const result = await NearestStore.find(filter);
+  res.json(result);
+};
+
 module.exports = {
   getAllStores: ctrlWrapper(getAllStores),
+  getNearestStores: ctrlWrapper(getNearestStores),
 };
--- routes/api/ePharmacy.js
+++ routes/api/ePharmacy.js
@@ -6,4 +6,6 @@ const ctrl = require("../../controllers/ePharmacy");
 
 router.get("/stores", ctrl.getAllStores);
 
+router.get("/stores/nearest", ctrl.getNearestStores);
+
 module.exports = router;
