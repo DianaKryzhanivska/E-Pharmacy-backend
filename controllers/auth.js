a2c80ede9ff42e08e4359f924ef948f2d5e198b5
DianaKryzhanivska <di_parkhomenko@ukr.net>
2024-04-11 14:45:42 +0300

    auth login ctrl

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models/users");
 const httpError = require("../services/httpError");
 const ctrlWrapper = require("../services/ctrlWrapper");
+
+const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
   const { name, email, phone, password } = req.body;
@@ -27,6 +30,37 @@ const register = async (req, res) => {
   });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw httpError(401, "Email or password is wrong");
+  }
+
+  const passwordCompare = await bcryptjs.compare(password, user.password);
+
+  if (!passwordCompare) {
+    throw httpError(401, "Email or password is wrong");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+
+  await User.findByIdAndUpdate(user._id, { token }, { new: true });
+
+  res.json({
+    token,
+    user: {
+      email,
+    },
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
 };
